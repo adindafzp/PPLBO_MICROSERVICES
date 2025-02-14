055ce03d44c93ec42f6119140b617426fe8e901c
Adinda <adindafauziapus@gmail.com>
2025-02-15 00:08:35 +0700

    part 4 spring cloud openfeign and testing with wiremock

--- order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservices.order.service;
 
+import com.techie.microservices.order.client.InventoryClient;
 import com.techie.microservices.order.dto.OrderRequest;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
@@ -13,13 +14,23 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
+ 
     public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + "is not in stock");
+
+        }
     }
 
 }
